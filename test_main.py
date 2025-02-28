d14030b7fc275b4878f21591dec0fb5f3fc5b7e4
navalesnahuel <navalesnahuel@gmail.com>
2025-02-28 17:26:05 -0300

    Add initial CI/CD workflow with deps

---
+++ test_main.py
@@ -0,0 +1,12 @@
+from main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+def test_something():
+    assert 'Test' == "Test"
